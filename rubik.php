bacf21bcd4a87d9ed1a0b41d842a37e1465c0bad
Juan Movil <u0_a240@localhost>
2020-07-25 20:17:30 -0500

    First format function

--- rubik.php
+++ rubik.php
@@ -1,13 +1,20 @@
 <?php
- $rubik = array();
- $sides = array('A','B','C','D','E','F');
- function resetRubik(){
-  global $sides, $rubik;
+ function getOrderedRubik(){
+  $rubik = array();
+  $sides = array('A','B','C','D','E','F');
   foreach ($sides as $side){
    for ($i = 1;$i<10;$i++){ 
     $rubik[$side.$i] = $side.$i;
    }
   }
+  return $rubik;
  }
- resetRubik();
- var_dump($rubik);
+ function format($rubik){
+  echo "         |--------|" ."\n";
+  echo "         |" . $rubik['B1'] . "-" . $rubik['B2'] . "-" . $rubik['B3'] . "|" ."\n";
+  echo "         |" . $rubik['B4'] . "-" . $rubik['B5'] . "-" . $rubik['B6'] . "|" ."\n";
+  echo "         |" . $rubik['B7'] . "-" . $rubik['B8'] . "-" . $rubik['B9'] . "|" ."\n";
+ }
+ $rubik=getOrderedRubik();
+ format($rubik);
+// var_dump($rubik);
