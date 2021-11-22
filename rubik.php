7944968c6771d8f731fa1df0230b9db90a0e0397
Juan Velez <juanvelez@crontopia.com>
2021-11-22 12:37:22 -0500

    Colored Rubik, reengine sexy moves functions

--- rubik.php
+++ rubik.php
@@ -1,149 +1,144 @@
 <?php
+ $red="\e[31m";
+ $green="\e[32m"; 
+ $yellow="\e[33m"; 
+ $blue="\e[34m";
+ $magenta="\e[95m"; 
+ $white="\e[97m";
  function getOrderedRubik(){
+  global $red, $green, $yellow, $blue, $magenta, $white;
   $rubik = array();
-  $sides = array('A','B','C','D','E','F');
+  $sides = array('U','B','R','D','L','F');
   foreach ($sides as $side){
    for ($i = 1;$i<10;$i++){ 
-    $rubik[$side.$i] = $side.$i;
+    $rubik[$side.$i]['v'] = $side.$i;
+    switch ($side){
+      case 'U':
+         $rubik[$side.$i]['c'] =  $yellow;
+         break;
+      case 'B':
+         $rubik[$side.$i]['c'] =  $magenta;
+         break;
+      case 'R':
+         $rubik[$side.$i]['c'] =  $green;
+         break;
+      case 'D':
+         $rubik[$side.$i]['c'] =  $white;
+         break;
+      case 'L':
+         $rubik[$side.$i]['c'] =  $blue;
+         break;
+      case 'F':
+         $rubik[$side.$i]['c'] =  $red;
+         break;
+    }
    }
   }
   return $rubik;
  }
- function moveA($rubik){
+ function rubikMove($rubik, $posA,$posN){
+   $tempRubik = $rubik;
+    for ($i = 0;$i < count($posA);$i++){
+      $posActual = $posA[$i];
+      $posNew = $posN[$i];
+      $rubik[$posActual]['v'] = $tempRubik[$posNew]['v'];
+      $rubik[$posActual]['c'] = $tempRubik[$posNew]['c'];
+    }
+    return $rubik;
+ }
+ function moveU($rubik){
     // Rotate A Side
-   $tempRubik = $rubik;
-   $rubik['A1'] = $tempRubik['A7'];
-   $rubik['A2'] = $tempRubik['A4'];
-   $rubik['A3'] = $tempRubik['A1'];
-   $rubik['A3'] = $tempRubik['A1'];
-   $rubik['A6'] = $tempRubik['A2'];
-   $rubik['A9'] = $tempRubik['A3'];
-   $rubik['A7'] = $tempRubik['A9'];
-   $rubik['A8'] = $tempRubik['A6'];
-   $rubik['A4'] = $tempRubik['A8'];
-   // Shift other sides
-   $rubik['C1'] = $tempRubik['B7'];
-   $rubik['C4'] = $tempRubik['B8'];
-   $rubik['C7'] = $tempRubik['B9'];
-   $rubik['F1'] = $tempRubik['C7'];
-   $rubik['F2'] = $tempRubik['C4'];
-   $rubik['F3'] = $tempRubik['C1'];
-   $rubik['E3'] = $tempRubik['F1'];
-   $rubik['E6'] = $tempRubik['F2'];
-   $rubik['E9'] = $tempRubik['F3'];
-   $rubik['B7'] = $tempRubik['E9'];
-   $rubik['B8'] = $tempRubik['E6'];
-   $rubik['B9'] = $tempRubik['E3'];
+   $posToChange = array (
+      'U1','U2','U3','U6','U9','U7','U8','U4',
+      'R1','R4','R7','F1','F2','F3','L3','L6','L9',
+      'B7','B8','B9'
+   );
+   $posNewVal = array (
+      'U7','U4','U1','U2','U3','U9','U6','U8',
+      'B7','B8','B9','R7','R4','R1','F1','F2','F3',
+      'L9','L6','L3'
+   );
+   $rubik = rubikMove($rubik, $posToChange, $posNewVal);
    return $rubik;
  }
- function moveAPrime($rubik){
+ function moveUPrime($rubik){
    // Rotate A Side
-   $tempRubik = $rubik;
-   $rubik['A1'] = $tempRubik['A3'];
-   $rubik['A2'] = $tempRubik['A6'];
-   $rubik['A3'] = $tempRubik['A9'];
-   $rubik['A6'] = $tempRubik['A8'];
-   $rubik['A9'] = $tempRubik['A7'];
-   $rubik['A8'] = $tempRubik['A4'];
-   $rubik['A7'] = $tempRubik['A1'];
-   $rubik['A4'] = $tempRubik['A2'];
-   // Shift other sides
-   $rubik['C1'] = $tempRubik['F3'];
-   $rubik['C4'] = $tempRubik['F2'];
-   $rubik['C7'] = $tempRubik['F1'];
-   $rubik['F1'] = $tempRubik['E3'];
-   $rubik['F2'] = $tempRubik['E6'];
-   $rubik['F3'] = $tempRubik['E9'];
-   $rubik['E3'] = $tempRubik['B9'];
-   $rubik['E6'] = $tempRubik['B8'];
-   $rubik['E9'] = $tempRubik['B7'];
-   $rubik['B7'] = $tempRubik['C1'];
-   $rubik['B8'] = $tempRubik['C4'];
-   $rubik['B9'] = $tempRubik['C7'];
+   $posToChange = array (
+      'U1','U2','U3','U6','U9','U8','U7','U4',
+      'R1','R4','R7','F1','F2','F3','L3','L6','L9',
+      'B7','B8','B9'
+   );
+   $posNewVal = array (
+      'U3','U6','U9','U8','U7','U4','U1','U2',
+      'F3','F2','F1','L3','L6','L9','B9','B8','B7',
+      'R1','R4','R7'
+   );
+   $rubik = rubikMove($rubik, $posToChange, $posNewVal);
    return $rubik;
  }
- function moveC($rubik){
-   // Rotate C Side
-   $tempRubik = $rubik;
-   $rubik['C1'] = $tempRubik['C7'];
-   $rubik['C2'] = $tempRubik['C4'];
-   $rubik['C3'] = $tempRubik['C1'];
-   $rubik['C3'] = $tempRubik['C1'];
-   $rubik['C6'] = $tempRubik['C2'];
-   $rubik['C9'] = $tempRubik['C3'];
-   $rubik['C7'] = $tempRubik['C9'];
-   $rubik['C8'] = $tempRubik['C6'];
-   $rubik['C4'] = $tempRubik['C8'];
-   // Shift other sides
-   $rubik['B3'] = $tempRubik['A3'];
-   $rubik['B6'] = $tempRubik['A6'];
-   $rubik['B9'] = $tempRubik['A9'];
-   $rubik['D7'] = $tempRubik['B3'];
-   $rubik['D4'] = $tempRubik['B6'];
-   $rubik['D1'] = $tempRubik['B9'];
-   $rubik['F3'] = $tempRubik['D7'];
-   $rubik['F6'] = $tempRubik['D4'];
-   $rubik['F9'] = $tempRubik['D1'];
-   $rubik['A3'] = $tempRubik['F3'];
-   $rubik['A6'] = $tempRubik['F6'];
-   $rubik['A9'] = $tempRubik['F9'];
+ function moveR($rubik){
+   // Rotate R Side
+   $posToChange = array (
+      'R1','R2','R3','R6','R9','R7','R8','R4',
+      'B3','B6','B9','D7','D4','D1','F3','F6','F9',
+      'U3','U6','U9'
+   );
+   $posNewVal = array (
+      'R7','R4','R1','R2','R3','R9','R6','R8',
+      'U3','U6','U9','B3','B6','B9','D7','D4','D1',
+      'F3','F6','F9'
+   );
+   $rubik = rubikMove($rubik, $posToChange, $posNewVal);
    return $rubik;
  }
- function moveCPrime($rubik){
-   // Rotate C Side in reverse
-   $tempRubik = $rubik;
-   $rubik['C1'] = $tempRubik['C3'];
-   $rubik['C2'] = $tempRubik['C6'];
-   $rubik['C3'] = $tempRubik['C9'];
-   $rubik['C6'] = $tempRubik['C8'];
-   $rubik['C9'] = $tempRubik['C7'];
-   $rubik['C8'] = $tempRubik['C4'];
-   $rubik['C7'] = $tempRubik['C1'];
-   $rubik['C4'] = $tempRubik['C2'];
-   // Shift other sides
-   $rubik['B3'] = $tempRubik['D7'];
-   $rubik['B6'] = $tempRubik['D4'];
-   $rubik['B9'] = $tempRubik['D1'];
-   $rubik['D7'] = $tempRubik['F3'];
-   $rubik['D4'] = $tempRubik['F6'];
-   $rubik['D1'] = $tempRubik['F9'];
-   $rubik['F3'] = $tempRubik['A3'];
-   $rubik['F6'] = $tempRubik['A6'];
-   $rubik['F9'] = $tempRubik['A9'];
-   $rubik['A3'] = $tempRubik['B3'];
-   $rubik['A6'] = $tempRubik['B6'];
-   $rubik['A9'] = $tempRubik['B9'];
+ function moveRPrime($rubik){
+   // Rotate R Side in reverse
+   $posToChange = array (
+      'R1','R2','R3','R6','R9','R8','R7','R4',
+      'B3','B6','B9','D7','D4','D1','F3','F6','F9',
+      'U3','U6','U9'
+   );
+   $posNewVal = array (
+      'R3','R6','R9','R8','R7','R4','R1','R2',
+      'D7','D4','D1','F3','F6','F9','U3','U6','U9',
+      'B3','B6','B9'
+   );
+   $rubik = rubikMove($rubik, $posToChange, $posNewVal);
    return $rubik;
  }
  function sexyMove($rubik){
-   $rubik=moveC($rubik);
-   $rubik=moveA($rubik);
-   $rubik=moveCPrime($rubik);
-   $rubik=moveAPrime($rubik);
+   $rubik=moveR($rubik);
+   $rubik=moveU($rubik);
+   $rubik=moveRPrime($rubik);
+   $rubik=moveUPrime($rubik);
    return $rubik;
+ }
+ function printPos($rubik,$pos){
+   global $red, $green, $yellow, $blue, $magenta, $white;
+    return $rubik[$pos]['c'].$rubik[$pos]['v'].$white;
  }
  function format($rubik){
    echo "         |--------|" ."\n";
-   echo "         |" . $rubik['B1'] . "-" . $rubik['B2'] . "-" . $rubik['B3'] . "|" ."\n";
-   echo "         |" . $rubik['B4'] . "-" . $rubik['B5'] . "-" . $rubik['B6'] . "|" ."\n";
-   echo "         |" . $rubik['B7'] . "-" . $rubik['B8'] . "-" . $rubik['B9'] . "|" ."\n";
+   echo "         |" . printPos($rubik,'B1') . "-" . printPos($rubik,'B2') . "-" . printPos($rubik,'B3') . "|" ."\n";
+   echo "         |" . printPos($rubik,'B4') . "-" . printPos($rubik,'B5') . "-" . printPos($rubik,'B6') . "|" ."\n";
+   echo "         |" . printPos($rubik,'B7') . "-" . printPos($rubik,'B8') . "-" . printPos($rubik,'B9') . "|" ."\n";
    echo "|--------|--------|--------|--------|" ."\n";
-   echo "|" . $rubik['E1'] . "-" . $rubik['E2'] . "-" . $rubik['E3'] . "|";
-   echo $rubik['A1'] . "-" . $rubik['A2'] . "-" . $rubik['A3'];
-   echo "|" . $rubik['C1'] . "-" . $rubik['C2'] . "-" . $rubik['C3'] . "|";
-   echo $rubik['D1'] . "-" . $rubik['D2'] . "-" . $rubik['D3']. "|" . "\n";
-   echo "|" . $rubik['E4'] . "-" . $rubik['E5'] . "-" . $rubik['E6'] . "|";
-   echo $rubik['A4'] . "-" . $rubik['A5'] . "-" . $rubik['A6'];
-   echo "|" . $rubik['C4'] . "-" . $rubik['C5'] . "-" . $rubik['C6'] . "|";
-   echo $rubik['D4'] . "-" . $rubik['D5'] . "-" . $rubik['D6']. "|" . "\n";
-   echo "|" . $rubik['E7'] . "-" . $rubik['E8'] . "-" . $rubik['E9'] . "|";
-   echo $rubik['A7'] . "-" . $rubik['A8'] . "-" . $rubik['A9'];
-   echo "|" . $rubik['C7'] . "-" . $rubik['C8'] . "-" . $rubik['C9'] . "|";
-   echo $rubik['D7'] . "-" . $rubik['D8'] . "-" . $rubik['D9']. "|" . "\n";
+   echo "|" . printPos($rubik,'L1') . "-" . printPos($rubik,'L2') . "-" . printPos($rubik,'L3') . "|";
+   echo printPos($rubik,'U1') . "-" . printPos($rubik,'U2') . "-" . printPos($rubik,'U3');
+   echo "|" . printPos($rubik,'R1') . "-" . printPos($rubik,'R2') . "-" . printPos($rubik,'R3') . "|";
+   echo printPos($rubik,'D1') . "-" . printPos($rubik,'D2') . "-" . printPos($rubik,'D3'). "|" . "\n";
+   echo "|" . printPos($rubik,'L4') . "-" . printPos($rubik,'L5') . "-" . printPos($rubik,'L6') . "|";
+   echo printPos($rubik,'U4') . "-" . printPos($rubik,'U5') . "-" . printPos($rubik,'U6');
+   echo "|" . printPos($rubik,'R4') . "-" . printPos($rubik,'R5') . "-" . printPos($rubik,'R6') . "|";
+   echo printPos($rubik,'D4') . "-" . printPos($rubik,'D5') . "-" . printPos($rubik,'D6'). "|" . "\n";
+   echo "|" . printPos($rubik,'L7') . "-" . printPos($rubik,'L8') . "-" . printPos($rubik,'L9') . "|";
+   echo printPos($rubik,'U7') . "-" . printPos($rubik,'U8') . "-" . printPos($rubik,'U9');
+   echo "|" . printPos($rubik,'R7') . "-" . printPos($rubik,'R8') . "-" . printPos($rubik,'R9') . "|";
+   echo printPos($rubik,'D7') . "-" . printPos($rubik,'D8') . "-" . printPos($rubik,'D9'). "|" . "\n";
    echo "|--------|--------|--------|--------|" ."\n";
-   echo "         |" . $rubik['F1'] . "-" . $rubik['F2'] . "-" . $rubik['F3'] . "|" ."\n";
-   echo "         |" . $rubik['F4'] . "-" . $rubik['F5'] . "-" . $rubik['F6'] . "|" ."\n";
-   echo "         |" . $rubik['F7'] . "-" . $rubik['F8'] . "-" . $rubik['F9'] . "|" ."\n";
+   echo "         |" . printPos($rubik,'F1') . "-" . printPos($rubik,'F2') . "-" . printPos($rubik,'F3') . "|" ."\n";
+   echo "         |" . printPos($rubik,'F4') . "-" . printPos($rubik,'F5') . "-" . printPos($rubik,'F6') . "|" ."\n";
+   echo "         |" . printPos($rubik,'F7') . "-" . printPos($rubik,'F8') . "-" . printPos($rubik,'F9') . "|" ."\n";
    echo "         |--------|" ."\n";
  }
  $rubik=getOrderedRubik();
@@ -152,6 +147,6 @@
    $rubik=sexyMove($rubik);
  }
  
- //$rubik=moveCPrime($rubik);
+ //$rubik=moveRPrime($rubik);
  format($rubik);
  // var_dump($rubik);
